1a43d672c425fb2d16ccb2da8bd2da5d1c0459f7
ericsmith92 <ericsmth884@gmail.com>
2018-01-31 07:31:17 -0500

    a few changes to db files

--- app/sql/local.wp_usermeta.sql
+++ app/sql/local.wp_usermeta.sql
@@ -17,8 +17,8 @@ INSERT INTO `wp_usermeta` VALUES
 (13,1,"wp_user_level","10"),
 (14,1,"dismissed_wp_pointers",""),
 (15,1,"show_welcome_panel","1"),
-(16,1,"session_tokens","a:2:{s:64:\"affdb1439d089d10ddbeae6fb6b9f814bd7cd7badebb9901b2b15e7ca81850ed\";a:4:{s:10:\"expiration\";i:1517249095;s:2:\"ip\";s:9:\"127.0.0.1\";s:2:\"ua\";s:115:\"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36\";s:5:\"login\";i:1517076295;}s:64:\"89890f6e260378787f7231947a2da1e4d735c64aee17f9c2b1b842ff4159d088\";a:4:{s:10:\"expiration\";i:1517335769;s:2:\"ip\";s:9:\"127.0.0.1\";s:2:\"ua\";s:115:\"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36\";s:5:\"login\";i:1517162969;}}"),
-(17,1,"wp_dashboard_quick_press_last_post_id","3"),
+(16,1,"session_tokens","a:2:{s:64:\"89890f6e260378787f7231947a2da1e4d735c64aee17f9c2b1b842ff4159d088\";a:4:{s:10:\"expiration\";i:1517335769;s:2:\"ip\";s:9:\"127.0.0.1\";s:2:\"ua\";s:115:\"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36\";s:5:\"login\";i:1517162969;}s:64:\"69aa93aa3319309a8074c4561ef94091c6c785369def5184533c7aa9d28deaa7\";a:4:{s:10:\"expiration\";i:1517448163;s:2:\"ip\";s:9:\"127.0.0.1\";s:2:\"ua\";s:115:\"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36\";s:5:\"login\";i:1517275363;}}"),
+(17,1,"wp_dashboard_quick_press_last_post_id","42"),
 (18,1,"community-events-location","a:1:{s:2:\"ip\";s:12:\"192.168.95.0\";}"),
 (19,1,"managenav-menuscolumnshidden","a:5:{i:0;s:11:\"link-target\";i:1;s:11:\"css-classes\";i:2;s:3:\"xfn\";i:3;s:11:\"description\";i:4;s:15:\"title-attribute\";}"),
 (20,1,"metaboxhidden_nav-menus","a:1:{i:0;s:12:\"add-post_tag\";}");
